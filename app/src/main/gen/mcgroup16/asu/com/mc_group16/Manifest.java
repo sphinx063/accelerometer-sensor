fc3b48ea446d941d09ae7b8d55ac92a06ca6a946
sphinx063 <rinku.cse12@gmail.com>
2017-03-12 18:05:16 -0700

    Just do it

---
+++ app/src/main/gen/mcgroup16/asu/com/mc_group16/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package mcgroup16.asu.com.mc_group16;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
